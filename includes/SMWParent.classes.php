faa48b5b51b31b28da95d4edafae3319f66123da
toniher <toniher@cau.cat>
2016-10-11 16:29:50 +0200

    test tree

--- includes/SMWParent.classes.php
+++ includes/SMWParent.classes.php
@@ -38,6 +38,17 @@ class SMWParent {
 
 	}
 
+
+	public static function executeGetChildrenTree( $input ) {
+
+		self::$round = 0;
+
+		$childrenList = self::getElementTree( "children", $input['parent_text'], $input['children_type'], $input['link_properties'], $input['type_properties'], $input['level'], $input['print_properties'] );
+
+		return $childrenList;
+
+	}
+
 	private static function getElement( $type="parent", $targetText, $sourceType, $linkProperties, $typeProperties, $level=1, $printProperties ) {
 
 		global $wgSMWParentlimit;
@@ -157,6 +168,148 @@ class SMWParent {
 					}
 				}
 			}
+		}
+
+		// Returns an array of hashes
+		return $targetOut;
+
+	}
+
+	private static function getElementTree( $type="parent", $targetText, $sourceType, $linkProperties, $typeProperties, $level=1, $printProperties ) {
+
+		global $wgSMWParentlimit;
+
+		// After results query, we add parent round
+		if (! isset(self::$round) ) {
+			self::$round = 0;
+		}
+		self::$round++;
+
+		// Ancestors limit
+		if ( $wgSMWParentlimit < self::$round ) {
+			return array();
+		}
+
+		// Query -> current page
+		$targetList = array();
+		
+
+		foreach ( $linkProperties as $prop ) {
+
+			$printoutProperties = $printProperties;
+
+			if ( $type === "parent" ) {
+				array_unshift( $printoutProperties, $prop );
+				$results = self::getQueryResults( "[[$targetText]]", $printoutProperties, false );
+			} else {
+				$results = self::getQueryResults( "[[$prop::$targetText]]", $printoutProperties, true );
+			}
+
+			if ( $results->getCount() > 0 ) {
+				$targetList[ $prop ] = $array();
+			}
+
+			// In theory, there is only one row
+			while ( $row = $results->getNext() ) {
+
+				$start = 2; // Start point for counting printouts
+				$add = 0;
+
+				if ( $type === "parent" ) {
+					$targetCont = $row[1];
+				} else {
+					$targetCont = $row[0];
+					$start = 1;
+					$add = 1;
+				}
+
+				$numColumns = count( $row );
+
+				if ( !empty($targetCont) ) {
+
+					$pageEntry = null;
+
+					// We assume value is only a single page
+					while ( $obj = $targetCont->getNextObject() ) {
+
+						$pageEntry = $obj->getWikiValue();
+					}
+
+					$printKeys = array();
+					for ( $v = $start; $v < $numColumns; $v++ ) {
+
+						if ( array_key_exists( $v - 1, $printoutProperties ) && array_key_exists( $v + $add, $row ) ) {
+
+							$printKey = $printoutProperties[ $v - 1 ];
+							$valueCont = $row[ $v + $add ];
+
+							if ( $valueCont && !empty($valueCont) ) {
+								if ( count( $valueCont ) > 1 ) {
+									$list = array();
+									while ( $obj = $valueCont->getNextObject() ) {
+										// TODO: We might be interested in handling type here
+										array_push( $list, $obj->getWikiValue() );
+									}
+									$printKeys[$printKey] = $list;
+								} else {
+									while ( $obj = $valueCont->getNextObject() ) {
+										// TODO: We might be interested in handling type here
+										$printKeys[$printKey] = $obj->getWikiValue();
+									}
+								}
+							}
+
+						}
+					}
+
+					if ( $pageEntry ) {
+						$targetList[$prop][ $pageEntry ] = $printKeys;
+					}
+				}
+			}
+
+		}
+
+		// Final ones to retrieve
+		$targetOut = array();
+
+		foreach ( $targetList as $prop => $matches ) {
+
+			if ( count( $matches ) > 0 ) {
+				$targetOut[ $prop ] = array();
+			}
+
+			foreach ( $matches as $target => $content ) {
+	
+				// Children round
+				if ( ( is_numeric($sourceType) && $sourceType == $level ) || ( self::isEntryType( $target, $sourceType, $typeProperties ) ) ) {
+	
+					$struct = array();
+					$struct[ $target ] = $content;
+					array_push( $targetOut[ $prop ], $struct );
+			
+				} else {
+	
+					// We increase level here.
+					// TODO: Make up a tree
+	
+					$itera = $level + 1;
+					$temparray = self::getElement( $type, $target, $sourceType, $linkProperties, $typeProperties, $itera, $printProperties );
+					
+					if ( count( $temparray ) > 0 ) {
+						$targetOut[ $prop ][ $target ] = array();
+					}
+
+					foreach ( $temparray as $temp ) {
+					
+						if ( $temp != '' ) {
+	
+							array_push( $targetOut[ $prop ][ $target ], $temp );
+						}
+					}
+				}
+			}
+
 		}
 
 		// Returns an array of hashes
