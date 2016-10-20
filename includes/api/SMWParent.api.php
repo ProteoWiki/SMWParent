5e24401dee442939fccb655aa5be2a817eb1a313
toniher <toniher@cau.cat>
2016-10-20 12:04:52 +0200

    parent api

--- includes/api/SMWParent.api.php
+++ includes/api/SMWParent.api.php
@@ -5,9 +5,51 @@ class ApiSMWParent extends ApiBase {
 
 		$params = $this->extractRequestParams();
 
-		// Handle array
+		$output = array();
+
+		$input = array();
+		$input['child_text'] = $params['title'];
+		$input['parent_text'] = $params['title'];
+		$input['parent_type'] = $params['type'];
+		$input['children_type'] = $params['type'];
+
+		if ( array_key_exists( "link_properties", $params ) ) {
+			$input['link_properties'] = explode( ",", $params['link_properties'] );
+		} else {
+			global $wgSMWParentProps;
+			$input['link_properties'] = $wgSMWParentProps;
+		}
+
+		if ( array_key_exists( "type_properties", $params ) ) {
+			$input['type_properties'] = explode( ",", $params['type_properties'] );
+		} else {
+			global $wgSMWParentTypeProperty;
+			$input['type_properties'] = $wgSMWParentTypeProperty;
+		}
+
+		if ( array_key_exists( "print_properties", $params ) ) {
+			$input['print_properties'] = explode( ",", $params['print_properties'] );
+		} else {
+			global $wgSMWParentPrintProps;
+			$input['print_properties'] = $wgSMWParentPrintProps;
+		}
+
+		switch ( $params['retrieve'] )  {
+
+			case "parent":
+				$output = SMWParent::executeGetParent( $input );
+
+			case "children":
+				$output = SMWParent::executeGetChildren( $input );
+
+			case "tree":
+				$output = SMWParent::executeGetTree( $input );
+
+		}
 
 		// TODO, generate processing
+		$this->getResult()->addValue( null, $this->getModuleName(), array ( 'status' => "OK", 'content' => $output ) );
+
 		return true;
 
 	}
@@ -48,7 +90,7 @@ class ApiSMWParent extends ApiBase {
 	}
 	public function getParamDescription() {
 		return array(
-			'retrieve' => 'Either parent or children',
+			'retrieve' => 'Either parent, children or tree',
 			'title' => 'Actual starting page',
 			'type' => 'Level or page type',
 			'link_properties' => 'Properties used for linking',
